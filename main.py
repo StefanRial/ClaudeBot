03a6aec773fa4a2ae4051253e6b28a141e791d70
StefanRial <mail.stefanrial@gmail.com>
2022-11-05 10:24:31 +0100

    Image downloading, local embedding

--- main.py
+++ main.py
@@ -9,11 +9,13 @@ E-Mail: mail.stefanrial@gmail.com
 
 import discord
 import openai
+import urllib.request
+from datetime import datetime
 from configparser import ConfigParser
 from discord import app_commands
 
 file = "config.ini"
-config = ConfigParser()
+config = ConfigParser(interpolation=None)
 config.read(file)
 
 SERVER_ID = config["discord"]["server_id"]
@@ -21,10 +23,13 @@ DISCORD_API_KEY = config["discord"][str("api_key")]
 OPENAI_ORG = config["openai"][str("organization")]
 OPENAI_API_KEY = config["openai"][str("api_key")]
 
+FILE_PATH = config["settings"][str("file_path")]
+FILE_NAME_FORMAT = config["settings"][str("file_name_format")]
+
 SIZE_LARGE = "1024x1024"
 SIZE_MEDIUM = "512x512"
 SIZE_SMALL = "256x256"
-SIZE_DEFAULT = SIZE_LARGE
+SIZE_DEFAULT = config["settings"][str("default_size")]
 
 GUILD = discord.Object(id=SERVER_ID)
 
@@ -49,6 +54,13 @@ openai.api_key = OPENAI_API_KEY
 openai.Model.list()
 
 
+def download_image(url: str):
+    file_name = f"{datetime.now().strftime(FILE_NAME_FORMAT)}.jpg"
+    full_path = f"{FILE_PATH}{file_name}"
+    urllib.request.urlretrieve(url, full_path)
+    return file_name
+
+
 @client.event
 async def on_ready():
     print(f"We have logged in as {client.user}")
@@ -58,6 +70,7 @@ async def on_ready():
 @app_commands.describe(prompt="Description of the image that Claude should generate")
 async def claude(interaction: discord.Interaction, prompt: str):
     mention = interaction.user.mention
+    channel = interaction.channel
     await interaction.response.defer()
 
     size = SIZE_DEFAULT
@@ -75,10 +88,15 @@ async def claude(interaction: discord.Interaction, prompt: str):
 
     response = openai.Image.create(prompt=prompt, n=1, size=size)
     image_url = response["data"][0]["url"]
+    image_name = download_image(image_url)
+    image_path = f"{FILE_PATH}{image_name}"
 
+    file = discord.File(image_path, filename=image_name)
     embed = discord.Embed(title=prompt)
-    embed.set_image(url=image_url)
+    embed.set_image(url=f"attachment://{image_name}")
 
-    await interaction.edit_original_response(content=f"{mention} Here is your result", embed=embed)
+    await channel.send(file=file, content=f"{mention} Here is your result", embed=embed)
+    await interaction.delete_original_response()
+
 
 client.run(DISCORD_API_KEY)
